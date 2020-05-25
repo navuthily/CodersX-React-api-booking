58f4514e870924c15c65cce15ffc9d7e382267ff
lyna <vuthilyna304@gmail.com>
2020-05-25 23:00:23 +0700

    story book

--- src/stories/Books.js
+++ src/stories/Books.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import App from '../App';
-import {withInfo} from '@storybook/addon-info'
-import {storiesOf} from'@storybook/react'
-export default { title: 'App' };
+import {
+  withInfo
+} from '@storybook/addon-info'
+import {
+  storiesOf
+} from '@storybook/react'
+//export default { title: 'App' };
 
-export const withText = () => <App/>;
+storiesOf('App', module)
+  .add(
+    'default',
+    () => ( <App/> )
+  )
